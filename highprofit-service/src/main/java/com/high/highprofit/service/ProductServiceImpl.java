cfb4be9ed9a9d3c04fee209cd5b6c361400d7334
high <2303006873@qq.com>
2022-11-24 15:57:57 +0800

    实现每日更新的动态利率展示

--- highprofit-service/src/main/java/com/high/highprofit/service/ProductServiceImpl.java
+++ highprofit-service/src/main/java/com/high/highprofit/service/ProductServiceImpl.java
@@ -2,6 +2,10 @@ package com.high.highprofit.service;
 
 import com.high.highprofit.mapper.ProductMapper;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 产品相关业务实现类
@@ -14,12 +18,21 @@ import org.apache.dubbo.config.annotation.DubboService;
 public class ProductServiceImpl implements ProductService{
     private final ProductMapper productMapper;
 
-    public ProductServiceImpl(ProductMapper productMapper) {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public ProductServiceImpl(ProductMapper productMapper, RedisTemplate<String, Object> redisTemplate) {
         this.productMapper = productMapper;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
     public String getAvgRate() {
-        return productMapper.getAvgRate();
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        String avgRate = (String) valueOperations.get("avgRate");
+        if (avgRate == null || avgRate.isEmpty()) {
+            avgRate = productMapper.getAvgRate();
+            valueOperations.set("avgRate", avgRate, 1, TimeUnit.DAYS);
+        }
+        return avgRate;
     }
 }
